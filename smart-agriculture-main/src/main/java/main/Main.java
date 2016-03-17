34b2104336522c38eb7365cc7cb80d115d26e171
Gökalp Gürbüzer <gokalp.gurbuzer@yandex.com>
2016-03-17 19:12:46 +0200

    Hello Main

---
+++ smart-agriculture-main/src/main/java/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+/**
+ * @author Gökalp Gürbüzer (ggurbuzer@qualist.com)
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!!");
+    }
+}
